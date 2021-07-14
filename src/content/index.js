aa4b466f24b33f4bb0d492746fc3863286b86681
henry <cksgod16@gmail.com>
2021-07-14 16:52:04 +0900

    Update

--- src/content/index.js
+++ src/content/index.js
@@ -5,77 +5,30 @@
  * @param { user } : array
  **/
 
-let wrapper;
-let message;
-let user;
-
-window.addEventListener('load', function () {
-  message = document.getElementsByClassName('text-fragment');
-  user = document.getElementsByClassName('chat-author__display-name');
-
-  console.log('user :', user, 'message :', message, message.innerText);
-});
+window.addEventListener('load', function () {});
 
 chrome.runtime.onMessage.addListener((request, sender, response) => {
   if (request.message) {
-    filter(request.message);
-  }
-});
-
-/* ====================================================
-                      filter message
-   ==================================================== */
-function filter(message) {
-  let chat = document.getElementsByClassName('text-fragment');
-
-  for (let content of chat) {
-    let text = content.innerHTML;
-
-    // if (message.includes(text)) {
-    //   return content.replace(/<span>/g, '<span class="block">');
-    // }
-  }
-}
-
-/* ====================================================
+    /* ====================================================
                       observe message
-   ==================================================== */
-let observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    console.log('mutation', mutation.type);
-  });
-});
+    ==================================================== */
 
-if (message) {
-  [...message].map((node) => {
-    console.log('@@@@@@", node');
-
-    observer.observe(node, {
-      attributes: true,
-      childList: true,
-      characterData: true,
-      subtree: true,
-      attributeOldValue: true,
-      attributeFilter: true,
-      characterDataOldValue: true,
+    var observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        mutation.addedNodes.forEach((chat) => {
+          if (chat.innerText && chat.innerText.includes(request.message)) {
+            return (chat.innerText = ' ');
+          }
+        });
+      });
     });
-  });
-}
+  }
 
-// when clicked stop button
-// observer.disconnect();
+  const config = {
+    childList: true,
+    subtree: true,
+  };
 
-/* ====================================================
-                 create wrapper element
-   ==================================================== */
-function createWrapper() {
-  let chat = document.getElementsByClassName(
-    'sc-AxjAm gwkXPf chat-scrollable-area__message-container',
-  );
-
-  wrapper = document.createElement('div');
-  wrapper.id = 'observer';
-
-  let parent = chat[0].firstChild;
-  chat.insterBefore(wrapper, parent);
-}
+  // 특정 url에서만 동작하도록 변경
+  observer.observe(document.body, config);
+});
